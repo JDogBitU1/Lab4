f4fc64d6ce362c9092128a82105b321c570a0e10
JDogBitU1 <38334542+JDogBitU1@users.noreply.github.com>
2019-02-18 19:18:56 -0500

    Lab4 - Monday: ChatRoom Activity not working, 3 other apps working. Commented out the auto-gene constructors and attempting to do the article

--- app/src/main/java/com/example/lab4/ListViewChat.java
+++ app/src/main/java/com/example/lab4/ListViewChat.java
@@ -1,21 +1,149 @@
 package com.example.lab4;
 
-import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.SpinnerAdapter;
+import android.app.Activity;
+import android.app.AlertDialog;
+
+public class ListViewChat extends ProfileActivity {
+
+        AlertDialog alertDialogStores;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_listview);
+
+            // a button to show the pop up with a list view
+            View.OnClickListener handler = new View.OnClickListener(){
+                public void onClick(View v) {
+                    switch (v.getId()) {
+
+                        case R.id.mobile_list:
+                            showPopUp();
+                            break;
+                    }
+                }
+            };
+
+            findViewById(R.id.mobile_list).setOnClickListener(handler);
+
+        }
+
+        public void showPopUp(){
+
+            // add your items, this can be done programatically
+            // your items can be from a database
+            ObjectItem[] ObjectItemData = new ObjectItem[20];
+
+            ObjectItemData[0] = new ObjectItem(91, "Mercury");
+            ObjectItemData[1] = new ObjectItem(92, "Watson");
+            ObjectItemData[2] = new ObjectItem(93, "Nissan");
+            ObjectItemData[3] = new ObjectItem(94, "Puregold");
+            ObjectItemData[4] = new ObjectItem(95, "SM");
+            ObjectItemData[5] = new ObjectItem(96, "7 Eleven");
+            ObjectItemData[6] = new ObjectItem(97, "Ministop");
+            ObjectItemData[7] = new ObjectItem(98, "Fat Chicken");
+            ObjectItemData[8] = new ObjectItem(99, "Master Siomai");
+            ObjectItemData[9] = new ObjectItem(100, "Mang Inasal");
+            ObjectItemData[10] = new ObjectItem(101, "Mercury 2");
+            ObjectItemData[11] = new ObjectItem(102, "Watson 2");
+            ObjectItemData[12] = new ObjectItem(103, "Nissan 2");
+            ObjectItemData[13] = new ObjectItem(104, "Puregold 2");
+            ObjectItemData[14] = new ObjectItem(105, "SM 2");
+            ObjectItemData[15] = new ObjectItem(106, "7 Eleven 2");
+            ObjectItemData[16] = new ObjectItem(107, "Ministop 2");
+            ObjectItemData[17] = new ObjectItem(108, "Fat Chicken 2");
+            ObjectItemData[18] = new ObjectItem(109, "Master Siomai 2");
+            ObjectItemData[19] = new ObjectItem(110, "Mang Inasal 2");
+
+            // our adapter instance
+            ArrayAdapterItem adapter = new ArrayAdapterItem(this, R.layout.activity_listview, ObjectItemData);
+
+            // create a new ListView, set the adapter and item click listener
+            ListView listViewItems = new ListView(this);
+            listViewItems.setAdapter(adapter);
+            listViewItems.setOnItemClickListener(new OnItemClickListenerListViewItem());
+
+            // put the ListView in the pop up
+            alertDialogStores = new AlertDialog.Builder(ListViewChat.this)
+                    .setView(listViewItems)
+                    .setTitle("Stores")
+                    .show();
+
+        }
+
+    }
 
 
 
 
-public class ListViewChat extends SpinnerAdapter <ListView> {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       /*
+
 
 
         public void onCreate(Bundle savedInstanceState) {
+
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_listview);
 
@@ -26,10 +154,11 @@ public class ListViewChat extends SpinnerAdapter <ListView> {
 
             // ORIGINAL: adapter = new ArrayAdapter<String>(ListViewChat.this, R.layout.activity_listview, R.id.mobile_list, mobileArray);
             ListView listView = (ListView) findViewById(R.id.mobile_list);
+            ListAdapter adapter;
             listView.setAdapter(adapter);
-
-
-            SpinnerAdapter adapter = new SpinnerAdapter(ListViewChat.this, R.layout.activity_listview, R.id.mobile_list, mobileArray) {
+*/
+/*
+            SpinnerAdapter adapter = new SpinnerAdapter(new AdapterView.adapter) {
                 @Override
                 public View getDropDownView(int position, View convertView, ViewGroup parent) {
                     return null;
@@ -84,10 +213,11 @@ public class ListViewChat extends SpinnerAdapter <ListView> {
                 public boolean isEmpty() {
                     return false;
                 }
-            };
+            };}
 
         }
 
+/*
     @Override
     public View getDropDownView(int position, View convertView, ViewGroup parent) {
         return null;
